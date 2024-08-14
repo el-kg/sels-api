6fbf9377a69807416dc4e758737200259aa49a62
el-kg <kirillgalkin8@gmail.com>
2024-08-14 10:57:46 +0700

    initial commit

---
+++ src/main/java/com/selsup/sels_api/SelsApiApplication.java
@@ -0,0 +1,13 @@
+package com.selsup.sels_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelsApiApplication.class, args);
+    }
+
+}
